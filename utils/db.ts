9ed2648b936e07754dd7b85e820664bd78745a9f
shahd <shahd@example.com>
2026-01-04 10:05:44 +0300

    therd try

--- utils/db.ts
+++ utils/db.ts
@@ -1,17 +1,22 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+  return new PrismaClient({
+    datasources: {
+      db: {
+        // نمرر الرابط مباشرة من البيئة، هذا سيحل مشكلة الـ Build والـ TypeScript
+        url: process.env.DATABASE_URL,
+      },
+    },
+  })
+}
 
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined;
-};
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
 
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+export default prisma
 
-export default prisma;
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
